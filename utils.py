04ad8715519d2a841a9a70c871c0d93c7c068309
yashashree0705 <166916867+yashashree0705@users.noreply.github.com>
2025-09-16 20:52:46 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,12 @@
-# utils.py
 import os
 import pandas as pd
 
-# Central location for logs.csv
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_FILE = os.path.join(BASE_DIR, "logs.csv")
 
 def init_logs():
-    """Create logs.csv if it doesn't exist"""
-    if not os.path.exists(LOG_FILE):
+    """Create logs.csv with headers if it doesn't exist or is empty"""
+    if not os.path.exists(LOG_FILE) or os.path.getsize(LOG_FILE) == 0:
         df_init = pd.DataFrame(columns=[
             "user_id", "date", "period",
             "fan_hours", "light_hours", "ac_hours", "charger_hours", "washing_cycles",
